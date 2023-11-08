89d2478a1a4a6214d63f683fefc982d36c03a07d
Timothy M. Wyatt <tmw@appcensus.io>
2023-11-08 13:51:18 -0500

    Use https-only api to pull OSV ecosystems rather than gcloud.

--- src/bomsquad/vulndb/client/osv.py
+++ src/bomsquad/vulndb/client/osv.py
@@ -1,13 +1,11 @@
 import json
 import logging
-from functools import cached_property
 from tempfile import NamedTemporaryFile
 from typing import Generator
-from typing import Optional
 from typing import Sequence
 from zipfile import ZipFile
 
-from google.cloud import storage
+import requests
 
 from bomsquad.vulndb.model.openssf import OpenSSF
 
@@ -15,6 +13,7 @@ logger = logging.getLogger(__name__)
 
 
 class OSV:
+    OSV_BASE = "https://osv-vulnerabilities.storage.googleapis.com"
     ECOSYSTEMS: Sequence[str] = (
         "Go",
         "npm",
@@ -38,15 +37,15 @@ class OSV:
 
     bucket: str = "osv-vulnerabilities"
 
-    @cached_property
-    def _gcs(self) -> storage.Client:
-        return storage.Client()
-
     def all(self, ecosystem: str) -> Generator[OpenSSF, None, None]:
-        gcs = self._gcs
         with NamedTemporaryFile(mode="wb") as fh:
-            blob = gcs.bucket(self.bucket).blob(f"{ecosystem}/all.zip")
-            blob.download_to_file(fh)
+            r = requests.get(f"{self.OSV_BASE}/{ecosystem}/all.zip")
+            if r.status_code != 200:
+                r.raise_for_status()
+
+            for chunk in r.iter_content(chunk_size=64 * 1024):
+                fh.write(chunk)
+
             fh.seek(0)
             with ZipFile(fh.name, mode="r") as zfh:
                 for entry in zfh.namelist():
@@ -55,15 +54,4 @@ class OSV:
                             jobj = json.load(content)
                             yield OpenSSF.parse_obj(jobj)
                         except Exception as e:
-                            logger.warning(f"{blob.name}: {e}")
-
-    def get(
-        self, ecosystem: str, offset: int = 0, limit: Optional[int] = None
-    ) -> Generator[OpenSSF, None, None]:
-        gcs = self._gcs
-        for blob in gcs.list_blobs(self.bucket, prefix=ecosystem):
-            try:
-                jobj = json.loads(blob.download_as_string())
-                yield OpenSSF.parse_obj(jobj)
-            except Exception as e:
-                logger.warning(f"{blob.name}: {e}")
+                            logger.warning(f"{entry}: {e}")
